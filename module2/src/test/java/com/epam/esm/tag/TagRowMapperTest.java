dab2cf48b4a4c954cb4c27fe00813133e3ec9611
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-27 11:33:55 +0300

    add tag row mapper test

--- module2/src/test/java/com/epam/esm/tag/TagRowMapperTest.java
+++ module2/src/test/java/com/epam/esm/tag/TagRowMapperTest.java
@@ -1,12 +1,42 @@
 package com.epam.esm.tag;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TagRowMapperTest {
 
+    @Mock
+    JdbcTemplate jdbcTemplate;
+    DataSource dataSource;
+    TagsDao tagsDao;
+
+    @BeforeEach
+    public void setup() {
+        dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .generateUniqueName(true)
+                .addScript("classpath:database.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        tagsDao = new TagsDaoImpl(jdbcTemplate,new TagRowMapper());
+    }
+
     @Test
     void mapRow() {
+        Optional<Tag> tagsDaoById = tagsDao.getById(1L);
+        assertTrue(tagsDaoById.isPresent());
+        assertEquals("name",tagsDaoById.get().getTagName());
+        assertEquals(1L,tagsDaoById.get().getId());
+
     }
 }

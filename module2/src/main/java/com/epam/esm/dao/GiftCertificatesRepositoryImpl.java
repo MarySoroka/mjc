8d54f36446411be3e442b3844ec32de47ab5cae5
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-29 00:51:11 +0300

    Change jdbcTemplate to namedJdbcTemplate. Refactor code for methods

--- module2/src/main/java/com/epam/esm/dao/GiftCertificatesRepositoryImpl.java
+++ module2/src/main/java/com/epam/esm/dao/GiftCertificatesRepositoryImpl.java
@@ -1,76 +1,67 @@
 package com.epam.esm.dao;
 
 import com.epam.esm.entity.GiftCertificate;
-import com.epam.esm.mapper.GiftCertificateRowMapper;
 import com.epam.esm.exception.DaoSaveException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class GiftCertificatesRepositoryImpl implements GiftCertificatesRepository {
     //change tag statement to certificate with group by
-    private static final String INSERT_CERTIFICATES_QUERY = "INSERT INTO gift_certificates.gift_certificate (name,description, price, create_date, last_update_date, duration ) values (?,?,?,?,?,?)";
-    private static final String UPDATE_CERTIFICATES_QUERY = "UPDATE gift_certificates.gift_certificate set name=? ,description=?, price=?, create_date=?, last_update_date=?, duration=? where id =?";
+    private static final String INSERT_CERTIFICATES_QUERY = "INSERT INTO gift_certificates.gift_certificate (name,description, price, create_date, last_update_date, duration ) values (:name,:description,:price,:createDate,:lastUpdateDate,:duration)";
+    private static final String UPDATE_CERTIFICATES_QUERY = "UPDATE gift_certificates.gift_certificate set name=:name ,description=:description, price=:price, create_date=:createDate, last_update_date=:lastUpdateDate, duration=:duration where id = :id";
 
-    private static final String DELETE_CERTIFICATES_QUERY = "DELETE FROM gift_certificates.gift_certificate WHERE id = ?";
+    private static final String DELETE_CERTIFICATES_QUERY = "DELETE FROM gift_certificates.gift_certificate WHERE id = :id";
     private static final String SELECT_ALL_CERTIFICATES_QUERY = "SELECT id,name,description, price, create_date, last_update_date, duration FROM gift_certificates.gift_certificate";
-    private static final String SELECT_CERTIFICATE_BY_ID_QUERY = "SELECT id,name,description, price, create_date, last_update_date, duration FROM gift_certificates.gift_certificate WHERE `id`=?";
-    private static final String SELECT_CERTIFICATE_BY_TAG_NAME_QUERY = "SELECT id,name,description, price, create_date, last_update_date, duration name FROM gift_certificates.gift_certificate WHERE `name`=?";
+    private static final String SELECT_CERTIFICATE_BY_ID_QUERY = "SELECT id,name,description, price, create_date, last_update_date, duration FROM gift_certificates.gift_certificate WHERE `id`= :id";
+    private static final String SELECT_CERTIFICATE_BY_TAG_NAME_QUERY = "SELECT id,name,description, price, create_date, last_update_date, duration name FROM gift_certificates.gift_certificate WHERE `name`= :name";
 
-    private final JdbcTemplate jdbcTemplate;
-    private final GiftCertificateRowMapper giftCertificateRowMapper;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
-    public GiftCertificatesRepositoryImpl(JdbcTemplate jdbcTemplate, GiftCertificateRowMapper giftCertificateRowMapper) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.giftCertificateRowMapper = giftCertificateRowMapper;
+    public GiftCertificatesRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
 
     @Override
     public Optional<GiftCertificate> getById(Long id) {
-        return Optional.ofNullable(
-                DataAccessUtils.singleResult(
-                        jdbcTemplate.query(
-                                SELECT_CERTIFICATE_BY_ID_QUERY,
-                                new Object[]{id},
-                                giftCertificateRowMapper)
-                )
-        );
+        Map<String, String> namedParameters = Collections.singletonMap("id", String.valueOf(id));
+        List<GiftCertificate> queryForObject = this.namedParameterJdbcTemplate.query(SELECT_CERTIFICATE_BY_ID_QUERY, namedParameters, new BeanPropertyRowMapper<>(GiftCertificate.class));
+        if (queryForObject.size() != 1)
+            return Optional.empty();
+        return Optional.ofNullable(queryForObject.get(0));
     }
 
     @Override
     public List<GiftCertificate> getAll() {
-        return jdbcTemplate.query(SELECT_ALL_CERTIFICATES_QUERY, giftCertificateRowMapper);
+        return namedParameterJdbcTemplate.query(SELECT_ALL_CERTIFICATES_QUERY, new BeanPropertyRowMapper<>(GiftCertificate.class));
     }
 
     @Override
     public boolean delete(Long id) {
-        return jdbcTemplate.update(DELETE_CERTIFICATES_QUERY, id) == 1;
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        return namedParameterJdbcTemplate.update(DELETE_CERTIFICATES_QUERY, namedParameters) != 0;
     }
 
 
     @Override
     public Long save(GiftCertificate giftCertificate) throws DaoSaveException {
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(
-                connection -> {
-                    PreparedStatement ps =
-                            connection.prepareStatement(INSERT_CERTIFICATES_QUERY, new String[]{"id"});
-                    setGiftCertificateRows(ps, giftCertificate);
-                    return ps;
-                },
-                keyHolder);
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(giftCertificate);
+        namedParameterJdbcTemplate.update(INSERT_CERTIFICATES_QUERY, namedParameters, keyHolder, new String[]{"id"});
         Number key = keyHolder.getKey();
         if (key == null) {
             throw new DaoSaveException("Couldn't save gift certificate");
@@ -79,31 +70,10 @@ public class GiftCertificatesRepositoryImpl implements GiftCertificatesRepositor
 
     }
 
-    private static int setGiftCertificateRows(PreparedStatement preparedStatement, GiftCertificate giftCertificate) throws SQLException {
-        int i = 0;
-        preparedStatement.setString(++i, giftCertificate.getCertificateName());
-        preparedStatement.setString(++i, giftCertificate.getCertificateDescription());
-        preparedStatement.setBigDecimal(++i, giftCertificate.getCertificatePrice());
-        preparedStatement.setDate(++i, Date.valueOf(giftCertificate.getCertificateCreateDate()));
-        preparedStatement.setDate(++i, Date.valueOf(giftCertificate.getCertificateLastUpdateDate()));
-        preparedStatement.setInt(++i, giftCertificate.getCertificateDuration());
-        return i;
-
-    }
-
     @Override
     public boolean update(GiftCertificate giftCertificate) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        int isUpdate = jdbcTemplate.update(
-                connection -> {
-                    PreparedStatement ps =
-                            connection.prepareStatement(UPDATE_CERTIFICATES_QUERY, new String[]{"id"});
-                    int i = setGiftCertificateRows(ps, giftCertificate);
-                    ps.setLong(++i, giftCertificate.getId());
-                    return ps;
-                },
-                keyHolder);
-
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(giftCertificate);
+        int isUpdate = namedParameterJdbcTemplate.update(UPDATE_CERTIFICATES_QUERY, namedParameters);
         return isUpdate > 0;
     }
 }
--- module2/src/main/java/com/epam/esm/dao/TagsRepositoryImpl.java
+++ module2/src/main/java/com/epam/esm/dao/TagsRepositoryImpl.java
@@ -2,72 +2,72 @@ package com.epam.esm.dao;
 
 import com.epam.esm.entity.Tag;
 import com.epam.esm.exception.DaoSaveException;
-import com.epam.esm.mapper.TagRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class TagsRepositoryImpl implements TagsRepository {
-    private static final String INSERT_TAG_QUERY = "INSERT INTO gift_certificates.tag (`name`) values (?)";
-    private static final String DELETE_TAG_QUERY = "DELETE FROM gift_certificates.tag WHERE id = ?";
+    private static final String INSERT_TAG_QUERY = "INSERT INTO gift_certificates.tag (`name`) values (:name)";
+    private static final String DELETE_TAG_QUERY = "DELETE FROM gift_certificates.tag WHERE id = :id";
     private static final String SELECT_ALL_TAGS_QUERY = "SELECT id, name FROM gift_certificates.tag";
-    private static final String SELECT_TAG_BY_ID_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE `id`=?";
-    private static final String SELECT_TAG_BY_NAME_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE `name`=?";
-    private final JdbcTemplate jdbcTemplate;
-    private final TagRowMapper tagRowMapper;
+    private static final String SELECT_TAG_BY_ID_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE id= :id";
+    private static final String SELECT_TAG_BY_NAME_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE `name`=: name";
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
-    public TagsRepositoryImpl(JdbcTemplate jdbcTemplate, TagRowMapper rowMapper) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.tagRowMapper = rowMapper;
+    public TagsRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+
     }
 
 
     @Override
     public Optional<Tag> getById(Long id) {
-        return Optional.ofNullable(
-                DataAccessUtils.singleResult(
-                        jdbcTemplate.query(
-                        SELECT_TAG_BY_ID_QUERY,
-                        new Object[]{id},
-                        tagRowMapper)
-                )
-        );
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        List<Tag> queryForObject = this.namedParameterJdbcTemplate.query(SELECT_TAG_BY_ID_QUERY, namedParameters, new BeanPropertyRowMapper<>(Tag.class));
+        if (queryForObject.size()!=1){
+            return Optional.empty();
+        }
+        return Optional.ofNullable(queryForObject.get(0));
+
     }
 
     @Override
     public List<Tag> getAll() {
-        return jdbcTemplate.query(SELECT_ALL_TAGS_QUERY, tagRowMapper);
+        return namedParameterJdbcTemplate.query(SELECT_ALL_TAGS_QUERY, new BeanPropertyRowMapper<>(Tag.class));
     }
 
     @Override
     public boolean delete(Long id) {
-        return jdbcTemplate.update(DELETE_TAG_QUERY, id) == 1;
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        return namedParameterJdbcTemplate.update(DELETE_TAG_QUERY, namedParameters) != 0;
+
+    }
+
+    @Override
+    public boolean update(Tag tag) {
+        throw new UnsupportedOperationException();
     }
 
 
     @Override
     public Long save(Tag tag) throws DaoSaveException {
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(
-                connection -> {
-                    PreparedStatement ps =
-                            connection.prepareStatement(INSERT_TAG_QUERY, new String[]{"id"});
-                    ps.setString(1, tag.getTagName());
-                    return ps;
-                },
-                keyHolder);
+        SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(tag);
+        namedParameterJdbcTemplate.update(INSERT_TAG_QUERY, namedParameters, keyHolder, new String[]{"id"});
         Number key = keyHolder.getKey();
         if (key == null) {
-            throw new DaoSaveException();
+            throw new DaoSaveException("Couldn't save tag certificate");
         }
         return key.longValue();
 

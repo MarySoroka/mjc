3a69243b8743c1bd4e9cd8708a688b3b0a863f7e
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-29 00:51:57 +0300

    Implements CRUDDao methods and add UnsupportedMethodException for update method

--- module2/src/main/java/com/epam/esm/dao/TagsRepository.java
+++ module2/src/main/java/com/epam/esm/dao/TagsRepository.java
@@ -1,42 +1,10 @@
 package com.epam.esm.dao;
 
 import com.epam.esm.entity.Tag;
-import com.epam.esm.exception.DaoSaveException;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * interface with CRD operations for Tag entity
  */
-public interface TagsRepository {
-    /**
-     * delete entity by key parameter
-     *
-     * @param id tag id
-     * @return if entity has been found and deleted - true, else false
-     */
-    boolean delete(Long id);
+public interface TagsRepository extends CRUDDao<Tag, Long> {
 
-    /**
-     * method save entity
-     *
-     * @param tag tag, that should be save
-     * @return if entity has been saved return generated id
-     * @throws DaoSaveException if generated id equals null
-     */
-    Long save(Tag tag) throws DaoSaveException;
-
-    /**
-     * @return all objects in specific db table
-     */
-    List<Tag> getAll();
-
-    /**
-     * method find entity by key parameter
-     *
-     * @param id tag id, which is using to find entity
-     * @return entity or Optional.empty(), if entity is not found by this key
-     */
-    Optional<Tag> getById(Long id);
 }

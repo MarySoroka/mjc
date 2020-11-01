b5aef7c7112b34c60d0a639f8424b0a2d1d6d4d0
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-01 16:37:22 +0300

    Change type of createDate and LastUpdateDate to LocalDateTime

--- module2/src/main/java/com/epam/esm/entity/GiftCertificate.java
+++ module2/src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -1,7 +1,8 @@
 package com.epam.esm.entity;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Set;
 
 public class GiftCertificate {
@@ -9,27 +10,50 @@ public class GiftCertificate {
     private String name;
     private String description;
     private BigDecimal price;
-    private LocalDate createDate;
-    private LocalDate lastUpdateDate;
+    private LocalDateTime createDate;
+    private LocalDateTime lastUpdateDate;
     private Integer duration;
     private Set<Tag> tags;
 
-    public GiftCertificate(Long id, String name, String description, BigDecimal price, LocalDate createDate, LocalDate lastUpdateDate, Integer duration) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.createDate = createDate;
-        this.lastUpdateDate = lastUpdateDate;
-        this.duration = duration;
+  public GiftCertificate(Long id, String name, String description, BigDecimal price,
+      LocalDateTime createDate, LocalDateTime lastUpdateDate, Integer duration) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.createDate = createDate;
+    this.lastUpdateDate = lastUpdateDate;
+    this.duration = duration;
+  }
+
+  public GiftCertificate() {
+
     }
 
-    public GiftCertificate() {
-
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GiftCertificate that = (GiftCertificate) o;
+    return Objects.equals(id, that.id) &&
+        Objects.equals(name, that.name) &&
+        Objects.equals(description, that.description) &&
+        Objects.equals(price, that.price) &&
+        Objects.equals(createDate, that.createDate) &&
+        Objects.equals(lastUpdateDate, that.lastUpdateDate) &&
+        Objects.equals(duration, that.duration);
+  }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, description, price, createDate, lastUpdateDate, duration);
+  }
 
-    public Long getId() {
+  public Long getId() {
         return id;
     }
 
@@ -61,21 +85,7 @@ public class GiftCertificate {
         this.price = price;
     }
 
-    public LocalDate getCreateDate() {
-        return createDate;
-    }
 
-    public void setCreateDate(LocalDate createDate) {
-        this.createDate = createDate;
-    }
-
-    public LocalDate getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(LocalDate lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
 
     public Integer getDuration() {
         return duration;
@@ -92,4 +102,20 @@ public class GiftCertificate {
     public void setTags(Set<Tag> tags) {
         this.tags = tags;
     }
+
+  public LocalDateTime getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(LocalDateTime createDate) {
+    this.createDate = createDate;
+  }
+
+  public LocalDateTime getLastUpdateDate() {
+    return lastUpdateDate;
+  }
+
+  public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+    this.lastUpdateDate = lastUpdateDate;
+  }
 }

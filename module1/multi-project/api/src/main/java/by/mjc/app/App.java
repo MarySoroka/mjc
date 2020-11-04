971b6e57d03dcd992573c847ad300f8bd0b32ef9
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-04 16:59:13 +0300

    delete useless folders

--- module1/multi-project/api/src/main/java/by/mjc/app/App.java
+++
@@ -1,9 +0,0 @@
-package by.mjc.app;
-
-import by.mjc.core.Utils;
-
-public class App {
-    public static void main(String[] args) {
-        Utils.isAllPositiveNumbers("12","79");
-    }
-}
--- module2/src/main/java/com/epam/esm/exception/ControllerEntityUpdateException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm.exception;
-
-public class ControllerEntityUpdateException extends Exception{
-
-  public ControllerEntityUpdateException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
-}

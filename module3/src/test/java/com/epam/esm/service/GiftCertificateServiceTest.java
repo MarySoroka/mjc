7a2240d6856e158b6027c7e3d59700861f29b184
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-05 16:21:38 +0300

    update GiftCertificateServiceTest tests(rename methods, add mockito stubbing)

--- module3/src/test/java/com/epam/esm/service/GiftCertificateServiceTest.java
+++ module3/src/test/java/com/epam/esm/service/GiftCertificateServiceTest.java
@@ -6,25 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.epam.esm.dao.GiftCertificateRepository;
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.entity.Tag;
 import com.epam.esm.exception.GiftCertificateNotFoundException;
+import com.epam.esm.exception.GiftCertificateServiceException;
 import com.epam.esm.exception.RepositoryDeleteException;
 import com.epam.esm.exception.RepositorySaveException;
+import com.epam.esm.exception.TagServiceException;
 import com.epam.esm.service.impl.GiftCertificateServiceImpl;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
-import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -40,51 +42,69 @@ class GiftCertificateServiceTest {
   @InjectMocks
   GiftCertificateServiceImpl giftCertificatesService;
 
-  GiftCertificate giftCertificate = new GiftCertificate(1L, "lala", "desk", new BigDecimal(12),
+  GiftCertificate expectedGiftCertificate = new GiftCertificate(1L, "lala", "desk",
+      new BigDecimal(12),
       ServiceUtils.getCurrentDateTime(), ServiceUtils.getCurrentDateTime(), 13);
+  Tag tag = new Tag(1L, "name");
 
 
   @Test
   void whenGetAllExistingCertificatesThenReturnOneCertificate() {
+    expectedGiftCertificate.setTags(Collections.singleton(tag));
+
+    when(giftCertificateRepository.getAllByQuery(anyMap()))
+        .thenReturn(Collections.singletonList(expectedGiftCertificate));
+    when(tagService.getTagsByCertificateId(anyLong())).thenReturn(Collections.singleton(tag));
+
     List<GiftCertificate> certificates = giftCertificatesService
         .getAllCertificates(new HashMap<>());
-    int size = certificates.size();
-    assertEquals(0, size);
+
+    assertEquals(1, certificates.size());
+    GiftCertificate giftCertificate = certificates.get(0);
+    assertEquals(expectedGiftCertificate, giftCertificate);
+    assertEquals(expectedGiftCertificate.getTags(), giftCertificate.getTags());
   }
 
   @Test
   void whenGetExistingCertificateByIdThenReturnCorrectCertificate()
       throws GiftCertificateNotFoundException {
-    when(giftCertificateRepository.getById(anyLong()))
-        .thenReturn(ofNullable(giftCertificate));
-    GiftCertificate certificateById = giftCertificatesService.getCertificateById(1L);
-    assertEquals(1L, certificateById.getId());
-    assertEquals("lala", certificateById.getName());
+    expectedGiftCertificate.setTags(Collections.singleton(tag));
 
+    when(giftCertificateRepository.getById(anyLong()))
+        .thenReturn(ofNullable(expectedGiftCertificate));
+    when(tagService.getTagsByCertificateId(anyLong())).thenReturn(Collections.singleton(tag));
+
+    GiftCertificate certificateById = giftCertificatesService.getCertificateById(1L);
+
+    assertEquals(expectedGiftCertificate, certificateById);
+    assertEquals(expectedGiftCertificate.getTags(), certificateById.getTags());
   }
 
   @Test
   void whenCreateCertificateCorrectThenReturnId()
-      throws RepositorySaveException {
+      throws RepositorySaveException, GiftCertificateServiceException, TagServiceException {
+    expectedGiftCertificate.setTags(Collections.singleton(tag));
     when(giftCertificateRepository.save(any())).thenReturn(1L);
-    ThrowingSupplier<Long> supplier = () -> giftCertificatesService
-        .createCertificate(giftCertificate);
-    assertDoesNotThrow(supplier);
+    doNothing().when(tagService).saveCertificateTag(any(Tag.class), anyLong());
+    when(giftCertificateRepository.getById(anyLong())).thenReturn(ofNullable(
+        expectedGiftCertificate));
+
+    assertEquals(expectedGiftCertificate,
+        giftCertificatesService.createCertificate(expectedGiftCertificate));
   }
 
   @Test
-  void whenDeleteExistingCertificateThenReturnTrue() throws RepositoryDeleteException {
+  void whenDeleteExistingCertificateThenDontThrowException() throws RepositoryDeleteException {
     doNothing().when(giftCertificateRepository).delete(anyLong());
-    Executable executable = () -> giftCertificatesService.deleteCertificate(1L);
-    assertDoesNotThrow(executable);
+    assertDoesNotThrow(() -> giftCertificatesService.deleteCertificate(1L));
   }
 
   @Test
-  void whenUpdateExistingCertificateThenReturnTrue() {
+  void whenUpdateExistingCertificateThenReturnDontThrowException() {
+    when(giftCertificateRepository.getById(anyLong())).thenReturn(ofNullable(
+        expectedGiftCertificate));
 
-    when(giftCertificateRepository.getById(anyLong())).thenReturn(ofNullable(giftCertificate));
-    Executable executable = () -> giftCertificatesService.updateCertificate(giftCertificate);
-    assertDoesNotThrow(executable);
+    assertDoesNotThrow(() -> giftCertificatesService.updateCertificate(expectedGiftCertificate));
   }
 
 

a9ffb093539bb2a39ecef39512a72ecd6078a329
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-06 14:17:06 +0300

    migrate from spring to spring bool

--- module3/src/main/java/com/epam/esm/config/AppConfig.java
+++ module3/src/main/java/com/epam/esm/config/AppConfig.java
@@ -7,6 +7,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;

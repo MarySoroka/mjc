6ec55021e960da50bac9de557c913bc40bc673df
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-11 18:33:30 +0300

    add equals methods for resource entities

--- module3/src/main/java/com/epam/esm/resource/GiftCertificateResource.java
+++ module3/src/main/java/com/epam/esm/resource/GiftCertificateResource.java
@@ -1,6 +1,7 @@
 package com.epam.esm.resource;
 
 import com.epam.esm.entity.GiftCertificate;
+import java.util.Objects;
 import org.springframework.hateoas.RepresentationModel;
 
 public class GiftCertificateResource extends RepresentationModel<GiftCertificateResource> {
@@ -9,5 +10,25 @@ public class GiftCertificateResource extends RepresentationModel<GiftCertificate
 
   public GiftCertificateResource(GiftCertificate giftCertificate) {
     this.giftCertificate = giftCertificate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    GiftCertificateResource that = (GiftCertificateResource) o;
+    return Objects.equals(giftCertificate, that.giftCertificate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), giftCertificate);
   }
 }
--- module3/src/main/java/com/epam/esm/resource/OrderResource.java
+++ module3/src/main/java/com/epam/esm/resource/OrderResource.java
@@ -8,20 +8,43 @@ import com.epam.esm.controller.OrderController;
 import com.epam.esm.entity.Order;
 import com.epam.esm.exception.ControllerEntityNotFoundException;
 import com.epam.esm.exception.ResourceBuildException;
+import java.util.Objects;
 import org.springframework.hateoas.RepresentationModel;
 
 public class OrderResource extends RepresentationModel<OrderResource> {
+
   private final Order order;
 
-  public OrderResource(Order order)  {
+  public OrderResource(Order order) {
     try {
-    this.order = order;
-    Long giftCertificateId = order.getGiftCertificate();
-    add(linkTo(OrderController.class).withRel("orders"));
+      this.order = order;
+      Long giftCertificateId = order.getGiftCertificate();
+      add(linkTo(OrderController.class).withRel("orders"));
 
-      add(linkTo(methodOn(GiftCertificatesController.class).getCertificateById(giftCertificateId)).withRel("giftCertificates"));
+      add(linkTo(methodOn(GiftCertificatesController.class).getCertificateById(giftCertificateId))
+          .withRel("giftCertificates"));
     } catch (ControllerEntityNotFoundException e) {
       throw new ResourceBuildException("Resource exception : couldn't build order");
     }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    OrderResource that = (OrderResource) o;
+    return Objects.equals(order, that.order);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), order);
   }
 }

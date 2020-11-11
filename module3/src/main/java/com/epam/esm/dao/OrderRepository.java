2ec882262013b3b7f01cbb346f1dee98f5ba02ac
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-11 18:34:22 +0300

    add get all user's orders by id method

--- module3/src/main/java/com/epam/esm/dao/OrderRepository.java
+++ module3/src/main/java/com/epam/esm/dao/OrderRepository.java
@@ -1,7 +1,8 @@
 package com.epam.esm.dao;
 
 import com.epam.esm.entity.Order;
+import java.util.Set;
 
 public interface OrderRepository extends CRUDDao<Order, Long>{
-
+   Set<Order> getAllUserOrders(Long userId);
 }

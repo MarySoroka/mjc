eabf288947caa0c0ad8ed97e0eea0a33d35df3a8
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-11 18:33:30 +0300

    add equals methods for resource entities

--- module3/src/main/java/com/epam/esm/resource/UserResource.java
+++ module3/src/main/java/com/epam/esm/resource/UserResource.java
@@ -6,11 +6,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import com.epam.esm.controller.OrderController;
 import com.epam.esm.controller.UserController;
 import com.epam.esm.entity.User;
+import java.util.Objects;
 import org.springframework.hateoas.RepresentationModel;
 
 public class UserResource extends RepresentationModel<UserResource> {
-  private final User user;
 
+  private final User user;
 
   public UserResource(final User user) {
     this.user = user;
@@ -18,5 +19,25 @@ public class UserResource extends RepresentationModel<UserResource> {
     add(linkTo(UserController.class).withRel("user"));
     add(linkTo(methodOn(OrderController.class).getAllUserOrders(id)).withRel("orders"));
 
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    UserResource that = (UserResource) o;
+    return Objects.equals(user, that.user);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), user);
   }
 }

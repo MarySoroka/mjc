f7a3e5de9a586093b772d7384c6f2472bb77f751
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-17 19:52:17 +0300

    update hibernate properties

--- module3/src/main/java/com/epam/esm/RestApiAdvancedApp.java
+++ module3/src/main/java/com/epam/esm/RestApiAdvancedApp.java
@@ -2,8 +2,10 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.epam.esm.entity")
 public class RestApiAdvancedApp {
 
   public static void main(String[] args) {

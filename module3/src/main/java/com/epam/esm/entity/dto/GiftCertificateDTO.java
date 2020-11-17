aa8b807f05805d8e3af366d1da981bb3e808a95b
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-17 22:02:35 +0300

    create dto entities for save and update methods in controller

---
+++ module3/src/main/java/com/epam/esm/entity/dto/GiftCertificateDTO.java
@@ -0,0 +1,134 @@
+package com.epam.esm.entity.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
+
+public class GiftCertificateDTO {
+
+  private Long id;
+  private String name;
+  private String description;
+  private BigDecimal price;
+  private LocalDateTime createDate;
+  private LocalDateTime lastUpdateDate;
+  private Integer duration;
+  private Set<TagDTO> tags;
+  private Set<OrderDTO> orders;
+
+  public GiftCertificateDTO(Long id, String name, String description, BigDecimal price,
+      LocalDateTime createDate, LocalDateTime lastUpdateDate, Integer duration,
+      Set<TagDTO> tags, Set<OrderDTO> orders) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.createDate = createDate;
+    this.lastUpdateDate = lastUpdateDate;
+    this.duration = duration;
+    this.tags = tags;
+    this.orders = orders;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GiftCertificateDTO that = (GiftCertificateDTO) o;
+    return Objects.equals(id, that.id) &&
+        Objects.equals(name, that.name) &&
+        Objects.equals(description, that.description) &&
+        Objects.equals(price, that.price) &&
+        Objects.equals(createDate, that.createDate) &&
+        Objects.equals(lastUpdateDate, that.lastUpdateDate) &&
+        Objects.equals(duration, that.duration) &&
+        Objects.equals(tags, that.tags) &&
+        Objects.equals(orders, that.orders);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects
+        .hash(id, name, description, price, createDate, lastUpdateDate, duration, tags, orders);
+  }
+
+  public GiftCertificateDTO() {
+  }
+
+  public Set<OrderDTO> getOrders() {
+    return orders;
+  }
+
+  public void setOrders(Set<OrderDTO> orders) {
+    this.orders = orders;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public LocalDateTime getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(LocalDateTime createDate) {
+    this.createDate = createDate;
+  }
+
+  public LocalDateTime getLastUpdateDate() {
+    return lastUpdateDate;
+  }
+
+  public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+    this.lastUpdateDate = lastUpdateDate;
+  }
+
+  public Integer getDuration() {
+    return duration;
+  }
+
+  public void setDuration(Integer duration) {
+    this.duration = duration;
+  }
+
+  public Set<TagDTO> getTags() {
+    return tags;
+  }
+
+  public void setTags(Set<TagDTO> tags) {
+    this.tags = tags;
+  }
+}

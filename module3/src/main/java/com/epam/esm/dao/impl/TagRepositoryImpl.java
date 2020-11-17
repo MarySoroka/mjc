0e25fa1433883e042b1a0d3033c5d1885bf36fa8
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-17 19:51:44 +0300

    update tag repository using entity manager

--- module3/src/main/java/com/epam/esm/dao/impl/TagRepositoryImpl.java
+++ module3/src/main/java/com/epam/esm/dao/impl/TagRepositoryImpl.java
@@ -9,14 +9,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -25,23 +28,22 @@ public class TagRepositoryImpl implements TagRepository {
   private static final String INSERT_TAG_QUERY = "INSERT INTO gift_certificates.tag (`name`) values (:name)";
   private static final String INSERT_TAG_CERTIFICATE_QUERY = "INSERT INTO gift_certificates.certificate_tag (tag_id, certificate_id) values (:tagId, :certificateId)";
   private static final String DELETE_CERTIFICATE_TAG_QUERY = "DELETE FROM gift_certificates.certificate_tag WHERE tag_id = :tagId AND certificate_id = :certificateId";
-
   private static final String DELETE_TAG_QUERY = "DELETE FROM gift_certificates.tag WHERE id = :id";
   private static final String SELECT_ALL_TAGS_QUERY = "SELECT id, name FROM gift_certificates.tag ORDER BY id LIMIT :limit OFFSET :offset";
   private static final String SELECT_TAG_BY_ID_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE id= :id";
   private static final String SELECT_TAG_BY_NAME_QUERY = "SELECT id, name FROM gift_certificates.tag WHERE `name`= :name";
-  private static final String SELECT_TAG_BY_CERTIFICATE_ID_QUERY = "SELECT t.id, t.name FROM gift_certificates.tag t JOIN certificate_tag ct on t.id = ct.tag_id JOIN gift_certificate gc on gc.id = ct.certificate_id WHERE gc.id =:id";
-
+  private static final String SELECT_TAG_BY_CERTIFICATE_ID_QUERY = "SELECT t.id, t.name FROM gift_certificates.tag t JOIN gift_certificates.certificate_tag ct on t.id = ct.tag_id JOIN gift_certificates.gift_certificate gc on gc.id = ct.certificate_id WHERE gc.id =:id";
   private static final String SELECT_THE_MOST_WIDELY_USED_TAG_QUERY =
       "SELECT t.id, t.name FROM gift_certificates.`order` o " +
-          "JOIN gift_certificate gc on o.order_certificate_id = gc.id " +
-          "JOIN certificate_tag ct on gc.id = ct.certificate_id " +
-          "JOIN tag t on t.id = ct.tag_id " +
+          "JOIN gift_certificates.gift_certificate gc on o.order_certificate_id = gc.id " +
+          "JOIN gift_certificates.certificate_tag ct on gc.id = ct.certificate_id " +
+          "JOIN gift_certificates.tag t on t.id = ct.tag_id " +
           "WHERE o.cost = (SELECT max(cost) FROM gift_certificates.`order`) " +
           "GROUP BY gc.id " +
           "ORDER BY count(t.id) DESC";
-
   private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  @PersistenceContext
+  private EntityManager entityManager;
 
   @Autowired
   public TagRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -52,35 +54,32 @@ public class TagRepositoryImpl implements TagRepository {
 
   @Override
   public Optional<Tag> getById(Long id) {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
-    List<Tag> queryForObject = this.namedParameterJdbcTemplate
-        .query(SELECT_TAG_BY_ID_QUERY, namedParameters, new BeanPropertyRowMapper<>(Tag.class));
-    if (queryForObject.size() != 1) {
-      return Optional.empty();
-    }
-    return Optional.ofNullable(queryForObject.get(0));
+    return Optional.ofNullable(entityManager.find(Tag.class, id));
 
   }
 
   @Override
-  public List<Tag> getAll(Map<String,Integer> pagination) {
-
-    Integer limit = Integer.parseInt(String.valueOf(pagination.get("limit")));
-    Integer offset = Integer.parseInt(String.valueOf(pagination.get("offset")));
-    SqlParameterSource namedParameters = new MapSqlParameterSource("limit", limit).addValue("offset",
-        offset);
-    return namedParameterJdbcTemplate
-        .query(SELECT_ALL_TAGS_QUERY,namedParameters, new BeanPropertyRowMapper<>(Tag.class));
+  public List<Tag> getAll(Map<String, Integer> pagination) {
+    int limit = Integer.parseInt(String.valueOf(pagination.get("limit")));
+    int offset = Integer.parseInt(String.valueOf(pagination.get("offset")));
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Tag> criteriaQuery = criteriaBuilder.createQuery(Tag.class);
+    Root<Tag> from = criteriaQuery.from(Tag.class);
+    CriteriaQuery<Tag> select = criteriaQuery.select(from);
+    TypedQuery<Tag> typedQuery = entityManager.createQuery(select);
+    typedQuery.setFirstResult(offset);
+    typedQuery.setMaxResults(limit);
+    return typedQuery.getResultList();
   }
 
   @Override
   public void delete(Long id) throws RepositoryDeleteException {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
-    if (namedParameterJdbcTemplate.update(DELETE_TAG_QUERY, namedParameters) == 0) {
-      throw new RepositoryDeleteException(
-          "Repository exception: Couldn't delete tag entity with id : " + id);
+    Tag tag = entityManager.find(Tag.class, id);
+    if (entityManager.contains(tag)) {
+      entityManager.remove(tag);
+    } else {
+      throw new RepositoryDeleteException("Repository exception: couldn't delete tsg by id: " + id);
     }
-
   }
 
   @Override
@@ -90,16 +89,9 @@ public class TagRepositoryImpl implements TagRepository {
 
 
   @Override
-  public Long save(Tag tag) throws RepositorySaveException {
-    KeyHolder keyHolder = new GeneratedKeyHolder();
-    SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(tag);
-    namedParameterJdbcTemplate
-        .update(INSERT_TAG_QUERY, namedParameters, keyHolder, new String[]{"id"});
-    Number key = keyHolder.getKey();
-    if (key == null) {
-      throw new RepositorySaveException("Repository exception: Couldn't save tag certificate");
-    }
-    return key.longValue();
+  public Long save(Tag tag) {
+    entityManager.persist(tag);
+    return tag.getId();
 
   }
 
@@ -113,13 +105,7 @@ public class TagRepositoryImpl implements TagRepository {
 
   @Override
   public Optional<Tag> getTagByName(String tagName) {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("name", tagName);
-    List<Tag> queryForObject = this.namedParameterJdbcTemplate
-        .query(SELECT_TAG_BY_NAME_QUERY, namedParameters, new BeanPropertyRowMapper<>(Tag.class));
-    if (queryForObject.size() != 1) {
-      return Optional.empty();
-    }
-    return Optional.ofNullable(queryForObject.get(0));
+    return Optional.ofNullable(entityManager.find(Tag.class, tagName));
 
   }
 

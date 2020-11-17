97156fa12f322f8a6714fe190d80db9615dc9fa0
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-18 00:10:17 +0300

    create criteria sql requests for order repository

--- module3/src/main/java/com/epam/esm/dao/impl/OrderRepositoryImpl.java
+++ module3/src/main/java/com/epam/esm/dao/impl/OrderRepositoryImpl.java
@@ -2,101 +2,86 @@ package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.OrderRepository;
 import com.epam.esm.entity.Order;
+import com.epam.esm.entity.User;
 import com.epam.esm.exception.RepositoryDeleteException;
-import com.epam.esm.exception.RepositorySaveException;
-import com.epam.esm.exception.RepositoryUpdateException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OrderRepositoryImpl implements OrderRepository {
 
-  private static final String INSERT_ORDERS_QUERY = "INSERT INTO gift_certificates.order (user_id, timestamp, cost, order_certificate_id) values (:userId,:orderTimestamp,:cost,:orderCertificateId)";
-  private static final String UPDATE_ORDERS_QUERY = "UPDATE gift_certificates.order set user_id=:userId, timestamp=:orderTimestamp, cost=:cost, order_certificate_id=:orderCertificateId where id = :id";
-  private static final String DELETE_ORDER_QUERY = "DELETE FROM gift_certificates.`order` WHERE id = :id";
-  private static final String SELECT_ALL_ORDERS_QUERY = "SELECT id, user_id, timestamp, cost, order_certificate_id  FROM gift_certificates.`order` ORDER BY id LIMIT :limit OFFSET :offset";
-  private static final String SELECT_ORDER_BY_ID_QUERY = "SELECT id, user_id, timestamp, cost, order_certificate_id  FROM gift_certificates.`order`  WHERE id = :id";
-  private static final String SELECT_ORDER_BY_USER_ID_QUERY = "SELECT id, user_id, timestamp, cost, order_certificate_id  FROM gift_certificates.`order`  WHERE user_id = :userId ORDER BY id LIMIT :limit OFFSET :offset";
-  private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+  @PersistenceContext
+  private EntityManager entityManager;
 
-  @Autowired
-  public OrderRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-  }
 
   @Override
   public Optional<Order> getById(Long id) {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
-    List<Order> queryForObject = this.namedParameterJdbcTemplate
-        .query(SELECT_ORDER_BY_ID_QUERY, namedParameters,
-            new BeanPropertyRowMapper<>(Order.class));
-    if (queryForObject.size() != 1) {
-      return Optional.empty();
-    }
-    return Optional.ofNullable(queryForObject.get(0));
+    return Optional.ofNullable(entityManager.find(Order.class, id));
   }
 
   @Override
-  public List<Order> getAll(Integer limit, Integer offset) {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("limit", limit)
-        .addValue("offset", offset);
-    return namedParameterJdbcTemplate
-        .query(SELECT_ALL_ORDERS_QUERY, namedParameters, new BeanPropertyRowMapper<>(Order.class));
+  public List<Order> getAll(Map<String, Integer> pagination) {
+    int limit = Integer.parseInt(String.valueOf(pagination.get("limit")));
+    int offset = Integer.parseInt(String.valueOf(pagination.get("offset")));
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Order> criteriaQuery = criteriaBuilder
+        .createQuery(Order.class);
+    Root<Order> from = criteriaQuery.from(Order.class);
+    CriteriaQuery<Order> select = criteriaQuery.select(from);
+    TypedQuery<Order> typedQuery = entityManager.createQuery(select);
+    typedQuery.setFirstResult(offset);
+    typedQuery.setMaxResults(limit);
+    return typedQuery.getResultList();
 
   }
 
   @Override
   public void delete(Long id) throws RepositoryDeleteException {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
-    if (namedParameterJdbcTemplate.update(DELETE_ORDER_QUERY, namedParameters) == 0) {
-      throw new RepositoryDeleteException(
-          "Repository exception: Couldn't delete order entity with id : " + id);
+    Order order = entityManager.find(Order.class, id);
+    if (entityManager.contains(order)) {
+      entityManager.remove(order);
+    } else {
+      throw new RepositoryDeleteException("Repository exception: couldn't delete tsg by id: " + id);
     }
   }
 
   @Override
-  public void update(Order order) throws RepositoryUpdateException {
-    SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(order);
-    int isUpdate = namedParameterJdbcTemplate.update(UPDATE_ORDERS_QUERY, namedParameters);
-    if (isUpdate == 0) {
-      throw new RepositoryUpdateException(
-          "Repository exception: Couldn't update order with id : " + order
-              .getId());
-    }
+  public void update(Order order) {
+    entityManager.merge(order);
   }
 
   @Override
-  public Long save(Order order) throws RepositorySaveException {
-    KeyHolder keyHolder = new GeneratedKeyHolder();
-    SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(order);
-    namedParameterJdbcTemplate
-        .update(INSERT_ORDERS_QUERY, namedParameters, keyHolder, new String[]{"id"});
-    Number key = keyHolder.getKey();
-    if (key == null) {
-      throw new RepositorySaveException("Repository exception: Couldn't save order");
-    }
-    return key.longValue();
+  public Order save(Order order) {
+    entityManager.persist(order);
+    return order;
   }
 
   @Override
-  public Set<Order> getAllUserOrders(Long userId, Integer limit, Integer offset) {
-    SqlParameterSource namedParameters = new MapSqlParameterSource("limit", limit)
-        .addValue("offset",
-            offset)
-        .addValue("userId", userId);
-    return new HashSet<>(this.namedParameterJdbcTemplate
-        .query(SELECT_ORDER_BY_USER_ID_QUERY, namedParameters,
-            new BeanPropertyRowMapper<>(Order.class)));
+  public Set<Order> getAllUserOrders(Long userId, Map<String, Integer> pagination) {
+    int limit = Integer.parseInt(String.valueOf(pagination.get("limit")));
+    int offset = Integer.parseInt(String.valueOf(pagination.get("offset")));
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Order> criteriaQuery = criteriaBuilder
+        .createQuery(Order.class);
+    Root<Order> orderRoot = criteriaQuery.from(Order.class);
+    Root<User> userRoot = criteriaQuery.from(User.class);
+
+    CriteriaQuery<Order> select = criteriaQuery.select(orderRoot);
+    select.where(
+        criteriaBuilder.equal(userRoot.get("user_id"), userId)
+    );
+    TypedQuery<Order> typedQuery = entityManager.createQuery(select);
+    typedQuery.setFirstResult(offset);
+    typedQuery.setMaxResults(limit);
+    return new HashSet<>(typedQuery.getResultList());
   }
 }

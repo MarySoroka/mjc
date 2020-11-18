f97ad9c7edd3055959cb9b50a8634ecc6ab174c2
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-18 15:14:05 +0300

    using OrderDTO instead of Order

--- module3/src/main/java/com/epam/esm/service/OrderService.java
+++ module3/src/main/java/com/epam/esm/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.epam.esm.service;
 
 import com.epam.esm.entity.Order;
+import com.epam.esm.entity.dto.OrderDTO;
 import com.epam.esm.exception.EntityNotFoundException;
 import com.epam.esm.exception.RepositoryDeleteException;
 import com.epam.esm.exception.RepositorySaveException;
@@ -12,8 +13,8 @@ import java.util.Set;
 public interface OrderService {
    List<Order> getAllOrders(Map<String,Integer> pagination);
    Order getOrderById(Long id) throws EntityNotFoundException;
-   Order createOrder(Order order) throws RepositorySaveException, EntityNotFoundException;
+   Order createOrder(OrderDTO order) throws RepositorySaveException, EntityNotFoundException;
    void deleteOrder(Long id) throws RepositoryDeleteException;
-   void updateOrder(Order order) throws RepositoryUpdateException;
+   void updateOrder(OrderDTO order) throws RepositoryUpdateException;
    Set<Order> getAllUserOrders(Long userId, Map<String,Integer> pagination);
 }

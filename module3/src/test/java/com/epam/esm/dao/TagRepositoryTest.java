6769727820e33067cd9b31722f71f1c416527955
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-19 23:47:49 +0300

    Update tests

--- module3/src/test/java/com/epam/esm/dao/TagRepositoryTest.java
+++ module3/src/test/java/com/epam/esm/dao/TagRepositoryTest.java
@@ -1,53 +1,44 @@
 package com.epam.esm.dao;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.epam.esm.dao.impl.TagRepositoryImpl;
 import com.epam.esm.entity.Tag;
 import com.epam.esm.exception.RepositoryDeleteException;
 import com.epam.esm.exception.RepositorySaveException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import javax.sql.DataSource;
-import org.junit.jupiter.api.BeforeAll;
+import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class TagRepositoryTest {
 
-  private static final Map<String, Integer> pagination = new HashMap<String, Integer>(2);
-  private static TagRepository tagRepository;
-
-  @BeforeAll
-  public static void setup() {
-    DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-        .generateUniqueName(true)
-        .addScript("classpath:database.sql")
-        .addScript("classpath:test-data.sql")
-        .build();
-    NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
-        dataSource);
-    tagRepository = new TagRepositoryImpl(namedParameterJdbcTemplate);
-    pagination.put("limit", 10);
-    pagination.put("offset", 0);
-  }
+  private final Tag tag = new Tag(null, "name");
+  @Autowired
+  protected EntityManager entityManager;
+  @Autowired
+  private TagRepository tagRepository;
 
   @Test
-  void whenGetByIdExistingTagFromDatabaseThenReturnCorrectTag() {
-    Optional<Tag> tagsRepositoryById = tagRepository.getById(3L);
+  void whenGetByIdExistingTagFromDatabaseThenReturnCorrectTag() throws RepositorySaveException {
+    tagRepository.save(tag);
+    Optional<Tag> tagsRepositoryById = tagRepository.getById(1L);
     assertTrue(tagsRepositoryById.isPresent());
     Tag tag = tagsRepositoryById.get();
-    assertEquals("la3", tag.getName());
+    assertEquals(this.tag, tag);
   }
 
   @Test
@@ -58,8 +49,8 @@ class TagRepositoryTest {
 
   @Test
   void whenGetAllFromDatabaseThenReturnCorrectTagsCount() {
-    List<Tag> tags = tagRepository.getAll(pagination);
-    assertEquals(5, tags.size());
+    List<Tag> tags = tagRepository.getAll(10, 0);
+    assertEquals(1, tags.size());
   }
 
   @Test
@@ -75,54 +66,36 @@ class TagRepositoryTest {
 
   @Test
   void whenUpdateTagThenReturnException() {
-    Tag tag = new Tag();
-    assertThrows(UnsupportedOperationException.class, () ->
-        tagRepository.update(tag)
-    );
+    assertThrows(UnsupportedOperationException.class, () -> tagRepository.update(tag));
   }
 
 
   @Test
   void whenSaveTagCorrectThenReturnId() throws RepositorySaveException {
     Tag tag = new Tag(null, "tag");
-    assertEquals(6L, tagRepository.save(tag));
+    assertEquals(tag, tagRepository.save(tag));
   }
 
   @Test
-  void whenGetTagsByCertificateIdThenReturnCorrectTwoTags() {
-    Set<Tag> tagsByCertificateId = tagRepository.getTagsByCertificateId(1L);
+  void whenGetTagsByCertificateIdThenReturnCorrectOneTag() {
+    Set<Tag> tagsByCertificateId = tagRepository.getTagsByCertificateId(1L, 10, 0);
     assertEquals(1L, tagsByCertificateId.size());
   }
 
   @Test
   void whenGetTagByNameThenReturnCorrectTag() {
     Optional<Tag> tagByName = tagRepository.getTagByName("name");
-    assertTrue( tagByName.isPresent());
+    assertTrue(tagByName.isPresent());
     Tag tag = tagByName.get();
     assertEquals(1L, tag.getId());
   }
+
   @Test
   void whenGetTagByNotExistingNameThenReturnOptionalEmpty() {
     Optional<Tag> tagByName = tagRepository.getTagByName("ngg");
-    assertFalse( tagByName.isPresent());
+    assertFalse(tagByName.isPresent());
   }
 
-  @Test
-  void whenSaveCertificateTagThenDoesntThrowsException() {
-    assertDoesNotThrow(() -> tagRepository.saveCertificateTag(4L, 3L));
-  }
-
-  @Test
-  void whenDeleteExistingCertificateTagThenReturnTrue() throws RepositoryDeleteException {
-    tagRepository.deleteCertificateTag(1L,1L);
-    Set<Tag> tagsByCertificateId = tagRepository.getTagsByCertificateId(1L);
-    assertNotEquals(2L,tagsByCertificateId.size());
-  }
-
-  @Test
-  void whenDeleteNotExistingCertificateTagThenReturnException() {
-    assertThrows(RepositoryDeleteException.class, () -> tagRepository.deleteCertificateTag(100L,10L));
-  }
   @Test
   void whenGetTheMostWidelyUsedTagThenReturnCorrectTag() {
     Tag widelyUsedTag = tagRepository.getTheMostWidelyUsedTag();
--- module3/src/test/java/com/epam/esm/service/OrderServiceTest.java
+++ module3/src/test/java/com/epam/esm/service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -13,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 import com.epam.esm.dao.impl.OrderRepositoryImpl;
 import com.epam.esm.entity.Order;
+import com.epam.esm.entity.dto.OrderDTO;
 import com.epam.esm.exception.EntityNotFoundException;
 import com.epam.esm.exception.RepositoryDeleteException;
 import com.epam.esm.exception.RepositorySaveException;
@@ -29,11 +31,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
 
-  private static final Map<String, Integer> pagination = new HashMap<String, Integer>(2);
 
   @Mock
   OrderRepositoryImpl orderRepository;
@@ -42,16 +44,11 @@ class OrderServiceTest {
   OrderServiceImpl orderService;
   Order order = new Order(1L, 1L, ServiceUtils.getCurrentDateTime(), new BigDecimal(13), 1L);
 
-  @BeforeAll
-  public static void setup() {
-    pagination.put("limit", 10);
-    pagination.put("offset", 0);
-  }
 
   @Test
   void whenMockGetAllOrdersThenReturnOrder() {
-    when(orderRepository.getAll(eq(pagination))).thenReturn(Collections.singletonList(order));
-    assertEquals(1L, orderService.getAllOrders(pagination).size());
+    when(orderRepository.getAll(anyInt(),anyInt())).thenReturn(Collections.singletonList(order));
+    assertEquals(1L, orderService.getAllOrders(10,0).size());
   }
 
   @Test
@@ -67,16 +64,15 @@ class OrderServiceTest {
   }
 
   @Test
-  void whenMockCreateOrderThenDoesntThrowException() throws RepositorySaveException {
-    when(orderRepository.save(any(Order.class))).thenReturn(1L);
-    when(orderRepository.getById(anyLong())).thenReturn(ofNullable(order));
-    assertDoesNotThrow(() -> orderService.createOrder(order));
+  void whenMockCreateOrderThenDoesntThrowException() {
+    when(orderRepository.save(any(Order.class))).thenReturn(order);
+    assertDoesNotThrow(() -> orderService.createOrder(new OrderDTO(order)));
   }
 
   @Test
-  void whenMockCreateIncorrectOrderThenThrowsException() throws RepositorySaveException {
-    when(orderRepository.save(any(Order.class))).thenThrow(RepositorySaveException.class);
-    assertThrows(RepositorySaveException.class, () -> orderService.createOrder(order));
+  void whenMockCreateIncorrectOrderThenThrowsException() {
+    when(orderRepository.save(any(Order.class))).thenThrow(DataIntegrityViolationException.class);
+    assertThrows(DataIntegrityViolationException.class, () -> orderService.createOrder(new OrderDTO(order)));
   }
 
   @Test
@@ -93,21 +89,21 @@ class OrderServiceTest {
   }
 
   @Test
-  void whenMockUpdateExistingOrderThenDoesntThrowException() throws RepositoryUpdateException {
+  void whenMockUpdateExistingOrderThenDoesntThrowException() {
     doNothing().when(orderRepository).update(any(Order.class));
-    assertDoesNotThrow(() -> orderService.updateOrder(order));
+    assertDoesNotThrow(() -> orderService.updateOrder(new OrderDTO(order)));
   }
 
   @Test
-  void whenMockUpdateNotExistingOrderThenDoesntThrowException() throws RepositoryUpdateException {
-    doThrow(RepositoryUpdateException.class).when(orderRepository).update(any(Order.class));
-    assertThrows(RepositoryUpdateException.class, () -> orderService.updateOrder(order));
+  void whenMockUpdateNotExistingOrderThenDoesntThrowException() {
+    doThrow(DataIntegrityViolationException.class).when(orderRepository).update(any(Order.class));
+    assertThrows(DataIntegrityViolationException.class, () -> orderService.updateOrder(new OrderDTO(order)));
   }
 
   @Test
   void whenMockGetAllUserOrdersThenReturnOneOrder() {
-    when(orderRepository.getAllUserOrders(anyLong(), eq(pagination)))
+    when(orderRepository.getAllUserOrders(anyLong(), anyInt(),anyInt()))
         .thenReturn(Collections.singleton(order));
-    assertEquals(1L, orderService.getAllUserOrders(1L, pagination).size());
+    assertEquals(1L, orderService.getAllUserOrders(1L, 10,0).size());
   }
 }
